b32bf63ad7e2a60c3f11254c7efcbabff95ced27
s17kf <s17kf@users.noreply.github.com>
2023-12-04 20:42:41 +0100

    Day 2 done

--- solutions/solve2.py
+++ solutions/solve2.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 
 import common
-from enum import Enum, auto
-from collections import Counter
-
+import numpy
 
 CUBES = {
     "red": 12,
@@ -12,14 +10,12 @@ CUBES = {
 }
 
 
-def is_possible(sets):
-    for cubes_set in sets:
-        cubes = cubes_set.split(", ")
-        for cube in cubes:
-            count, color = cube.split(" ")
-            count = int(count)
-            if count > CUBES[color]:
-                return False
+def is_possible(words):
+    for i in range(0, len(words), 2):
+        value = int(words[i])
+        color = words[i + 1]
+        if value > CUBES[color]:
+            return False
     return True
 
 
@@ -28,11 +24,33 @@ def get_possible_games(lines):
     for line in lines:
         game, results = line.split(": ")
         game_id = int(game.split()[1])
-        sets = results.split("; ")
-        if not is_possible(sets):
+        results = results.replace(";", "")
+        results = results.replace(",", "")
+        words = results.split()
+        if not is_possible(words):
             continue
         possible_games.append(game_id)
     return possible_games
+
+
+def find_minimum_sets(lines):
+    powers = []
+    for line in lines:
+        _, results = line.split(": ")
+        minimum = {
+            "red": 0,
+            "green": 0,
+            "blue": 0
+        }
+        results = results.replace(";", "")
+        results = results.replace(",", "")
+        words = results.split()
+        for i in range(0, len(words), 2):
+            value = int(words[i])
+            color = words[i + 1]
+            minimum[color] = max(minimum[color], value)
+        powers.append(numpy.prod([minimum["red"], minimum["green"], minimum["blue"]]))
+    return powers
 
 
 input_lines = common.init_day(2)
@@ -40,10 +58,7 @@ if input_lines is None:
     exit(1)
 
 result1 = sum(get_possible_games(input_lines))
-
-
-
-result2 = 1
+result2 = sum(find_minimum_sets(input_lines))
 
 print(f"task1: {result1}")
 print(f"task2: {result2}")
