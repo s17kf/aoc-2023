764f4dc62acc293bca38bcc0e14f008b8523793a
s17kf <s17kf@users.noreply.github.com>
2023-12-07 23:47:59 +0100

    Improve solution template

--- solutions/solveTemplate.py
+++ solutions/solveTemplate.py
@@ -7,19 +7,21 @@ import numpy
 from common import numpy_matrix
 
 
+def do_part1(lines):
+    print(lines)
+    return "part 1"
+
+
+def do_part2(lines):
+    return "part2"
+
+
 input_lines = common.init_day(DAY_NUM)
 if input_lines is None:
     exit(1)
 
-
-
-
-
-
-
-
-result1 = 0
-result2 = 1
+result1 = do_part1(input_lines)
+result2 = do_part2(input_lines)
 
 print(f"task1: {result1}")
 print(f"task2: {result2}")
