1aa79fdcb7fd22fe8ce0a8b6e536ab0ae6253f38
s17kf <s17kf@users.noreply.github.com>
2023-12-08 10:27:47 +0100

    Day 8 - part 2 greedy solution
    Takes too much time

--- solutions/solve8.py
+++ solutions/solve8.py
@@ -12,25 +12,54 @@ DIR_INDICES = {
 }
 
 
-def do_part1(lines):
-    instructions = lines[0]
+def get_network_dict(lines):
     network_dict = dict()
     for line in lines[2:]:
         source, dests = line.split(" = ")
         dests = dests.strip("()").split(", ")
         network_dict[source] = dests
-    node = "AAA"
-    i = 0
+    return network_dict
+
+
+def do_part1(lines, start_node="AAA", end_condition=lambda node: node == "ZZZ"):
+    instructions = lines[0]
+    network_dict = get_network_dict(lines)
+    node = start_node
+    instruction = 0
     steps = 0
-    while node != "ZZZ":
+    while not end_condition(node):
         steps += 1
-        node = network_dict[node][DIR_INDICES[instructions[i]]]
-        i = (i + 1) % len(instructions)
+        node = network_dict[node][DIR_INDICES[instructions[instruction]]]
+        instruction = (instruction + 1) % len(instructions)
     return steps
 
 
 def do_part2(lines):
-    return "part2"
+    instructions = lines[0]
+    network_dict = get_network_dict(lines)
+    nodes = []
+    for node in network_dict.keys():
+        if node[2] == "A":
+            nodes.append(node)
+    instruction = 0
+    steps = 0
+    done = False
+    while not done:
+        # print(f"{steps}: {nodes}")
+        steps += 1
+        direction = DIR_INDICES[instructions[instruction]]
+        instruction = (instruction + 1) % len(instructions)
+        for i in range(len(nodes)):
+            node = nodes[i]
+            nodes[i] = network_dict[node][direction]
+        done = True
+        for node in nodes:
+            if node[2] != "Z":
+                done = False
+                break
+        # if steps == 10:
+        #     break
+    return steps
 
 
 input_lines = common.init_day(8)
@@ -38,7 +67,7 @@ if input_lines is None:
     exit(1)
 
 result1 = do_part1(input_lines)
-result2 = do_part2(input_lines)
-
 print(f"task1: {result1}")
+
+result2 = do_part2(input_lines)
 print(f"task2: {result2}")
